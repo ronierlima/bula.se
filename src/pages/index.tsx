40c7a1c2604e2a8c9310dc29f6b9db397d0580de
Ronier Lima <ronier.lim@gmail.com>
2021-02-24 10:26:28 -0300

    Migrando o projeto para Next.js

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import { CompleteChallenges } from "../components/CompleteChallenges";
+import { Countdown } from "../components/Countdown";
+import { ExperienceBar } from "../components/ExperienceBar";
+
+import Head  from "next/head";
+
+import { Profile } from "../components/Profile";
+
+import styles from "../styles/pages/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompleteChallenges />
+          <Countdown />
+        </div>
+        <div></div>
+      </section>
+    </div>
+  );
+}
